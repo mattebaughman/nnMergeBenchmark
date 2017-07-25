95b54d9926991fb48a1d6d7832aa93602e8a8bd1
mattebaughman <mbaughman123@gmail.com>
2017-07-25 13:58:57 -0500

    Delete runit.sh

--- runit.sh
+++
@@ -1,6 +0,0 @@
-sudo docker run --rm -ti crisbal/torch-rnn:base bash
-python scripts/preprocess.py \
---input_txt data/tiny-shakespeare.txt \
---output_h5 data/tiny-shakespeare.h5 \
---output_json data/tiny-shakespeare.json
-th train.lua -gpu -1
